a8c90aadf1bb0c7af98cbf85c3f33e42be673afd
vanessapereiracunha <vanessapereiracunha72@gmail.com>
2024-07-02 17:34:18 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -2,35 +2,43 @@
 Parse.initialize("uICi8xqYEQ7dpHbf2rGjuKjMnyYnFLTJBCHg056J", "jqu44b1D0lMX47pRUz72PC3HL8CovkAja8kW2QxP");
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
-
-
-document.getElementById('signup-button').addEventListener('click', function() {
+document.getElementById('signup-button').addEventListener('click', async function() {
     const username = document.getElementById('signup-username').value;
     const password = document.getElementById('signup-password').value;
-    const userType = document.getElementById('signup-userType').value; // Corrigindo ID e adicionando a variável aqui
-    const email = document.getElementById('signup-email').value; // Corrigindo ID e adicionando a variável aqui
-    signUp(username, password, userType, email); // Passando userType como um parâmetro para a função signUp
+    const userType = document.getElementById('signup-userType').value;
+    const email = document.getElementById('signup-email').value;
+
+    try {
+        await Parse.User.logOut();
+        signUp(username, password, userType, email);
+    } catch (error) {
+        console.error('Error while logging out user', error);
+        alert('Erro ao deslogar: ' + error.message);
+    }
 });
 
-// Alteração dos parâmetros da função signUp para incluir userType
+// Função de cadastro do usuário
 function signUp(username, password, userType, email) {
     var user = new Parse.User();
     user.set("username", username);
     user.set("password", password);
-    user.set("type", userType); // Definindo a propriedade "type" do usuário, nome do campo ajustado de "usertype" para "type"
-    user.set("email", email); // Definindo a propriedade "type" do usuário, nome do campo ajustado de "usertype" para "type"
+    user.set("type", userType);
+    user.set("email", email);
 
-    // Movendo o redirecionamento para dentro do .then, após sucesso no cadastro
     user.signUp().then(function(user) {
         console.log('User signed up', user);
+        document.getElementById('success-message').style.display = 'block';
+        document.getElementById('error-message').style.display = 'none';
         // Redirecionar para a página correspondente após um cadastro bem-sucedido
         if(user.get("type") === "professor") {
-            window.location.href = 'menu_professor.html'; // Modifique conforme necessário
+            window.location.href = 'menu_professor.html';
         } else if(user.get("type") === "aluno") {
-            window.location.href = 'menu_aluno.html'; // Modifique conforme necessário
+            window.location.href = 'menu_aluno.html';
         }
     }).catch(function(error) {
         console.error('Error while signing up user', error);
+        document.getElementById('success-message').style.display = 'none';
+        document.getElementById('error-message').style.display = 'block';
         alert('Erro ao registrar: ' + error.message);
     });
 }
